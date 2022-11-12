cdd99b50a4e45bfae3a8f5220f7fe16b52916487
ValberJunior <valberjr1992@gmail.com>
2022-11-12 12:59:18 -0300

    create new-user component

--- src/app/home/new-user/new-user.component.ts
+++ src/app/home/new-user/new-user.component.ts
@@ -1,4 +1,10 @@
+import { NewUserService } from './new-user.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IUser } from 'src/utils/interfaces';
+import { cpfValidator } from './cpf.validator';
+import { UserExistsService } from './user-exists.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-user',
@@ -7,9 +13,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  newUserForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private newUserService: NewUserService,
+    private userExistsService : UserExistsService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+    this, this.newUserForm = this.formBuilder.group({
+      fullName: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(100)
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.email,
+      ],
+      [
+        this.userExistsService.UserAlreadyExists()
+      ]
+      ],
+      phone: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(14)
+      ]
+      ],
+      cpf: ['', [
+        Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(14),
+        cpfValidator
+      ]
+      ],
+      password: ['', [
+        Validators.required, Validators.minLength(6)
+      ]]
+    })
   }
 
+  register() {
+    if (this.newUserForm.valid){
+    const newUser = this.newUserForm.getRawValue() as IUser;
+      if(this.newUserService.CreateUser(newUser)){
+        this.router.navigateByUrl('');
+      }else{
+        console.log("Ocorreu um erro")
+      }
+    }
+  }
 }
