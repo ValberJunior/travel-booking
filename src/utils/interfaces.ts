af99e26fa8a0622262cc1058ba457f954e9371e6
ValberJunior <valberjr1992@gmail.com>
2022-11-12 13:00:07 -0300

    update IUser

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,14 +1,13 @@
 export interface IUser {
   fullName: string;
-  username:string;
   email: string;
   phone: string;
-  password: string;
   cpf: string;
+  password: string;
 }
 
 export interface ILogin {
-  username: string;
+  email: string;
   password: string
 }
 
