4aae29fae398688eff3ff6451e3cc48494769a0e
ValberJunior <valberjr1992@gmail.com>
2022-11-12 12:59:57 -0300

    create common file

---
+++ src/utils/commons.ts
@@ -0,0 +1,21 @@
+export const CACHE = localStorage.getItem("Session");
+
+export const USERS = localStorage.getItem("Users");
+
+export const DEFAULT_USERS = [{
+  fullName: "Admin",
+  username: "Admin",
+  email: "Admin@teste.com",
+  phone: "(21)99999-8888",
+  password: "admin@2022",
+  cpf: "111.111.111-11",
+},
+{
+    fullName: "Valber",
+    username:"Valber",
+    email: "valber@teste.com",
+    phone: "(21)99999-8888",
+    password: "123456",
+    cpf: "111.111.111-11",
+  }
+];
