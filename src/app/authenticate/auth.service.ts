bd482c8c56871d1f2c87364ce545d32f1bba53e5
ValberJunior <valberjr1992@gmail.com>
2022-12-11 23:48:48 -0300

    add API variable

--- src/app/authenticate/auth.service.ts
+++ src/app/authenticate/auth.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { UserService } from './user/user.service';
 import { ILogin } from './../../utils/interfaces';
 import { HttpClient, HttpResponse } from '@angular/common/http';
@@ -5,7 +6,7 @@ import { Injectable } from '@angular/core';
 import { Observable} from 'rxjs';
 import { tap } from 'rxjs/operators';
 
-
+const API = environment.apiURL;
 @Injectable({
   providedIn: 'root'
 })
@@ -17,7 +18,7 @@ export class AuthService {
   ) { }
 
   authenticate(credential:ILogin): Observable<HttpResponse<any>>{
-    return this.httpClient.post('https://api-travel-booking.vercel.app/user/login',{
+    return this.httpClient.post(`${API}/user/login`,{
       email: credential.email,
       password: credential.password
     },
@@ -25,13 +26,16 @@ export class AuthService {
     ).pipe(
       tap((res)=>{
         const authToken = res.headers.get('authorization-token');
+        console.log("Se tiver token -->>",authToken);
         if(authToken !== "" && authToken !== null) return this.userService.saveToken(authToken);
         else{
          const body : Object | any = res.body;
          const id = body._id;
-         this.httpClient.get(`https://api-travel-booking.vercel.app/user/returnToken/${id}`).subscribe(
+         console.log("Se não tiver token, será gerado para o ID:",id)
+         this.httpClient.get(`${API}/user/returnToken/${id}`).subscribe(
           (res)=>{
             const token = JSON.stringify(res);
+            console.log("token gerado",token)
             return this.userService.saveToken(token);
           }
          )
